b74e96db11a37e5a17c7e9fb9f3a63c071f4073c
Lin-Guanguo <lin.guanguo2000@gmail.com>
2022-03-04 18:13:12 +0800

    hello Service Impl

--- src/example/mod.rs
+++ src/example/mod.rs
@@ -9,3 +9,20 @@ pub fn hello_service(reqeust: Bytes) -> (u32, Bytes) {
     reply.msg = format!("hello {}", reqeust.name);
     (0, reply.encode_to_vec().into())
 }
+
+pub trait HelloServer {
+    fn hello_service(&mut self, reqeust: Bytes) -> (u32, Bytes);
+}
+
+struct HelloServerImpl {
+    count: i32,
+}
+
+impl HelloServer for HelloServerImpl {
+    fn hello_service(&mut self, reqeust: Bytes) -> (u32, Bytes) {
+        let reqeust = HelloRequest::decode(&reqeust[..]).unwrap();
+        let mut reply = HelloReply::default();
+        reply.msg = format!("hello {}, count = {}", reqeust.name, self.count);
+        (0, reply.encode_to_vec().into())
+    }
+}
