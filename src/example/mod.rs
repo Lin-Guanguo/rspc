f133c371d90dcd047927b2ff4877f1b695f7f193
Lin-Guanguo <lin.guanguo2000@gmail.com>
2022-03-04 13:22:49 +0800

    make example better

--- src/example/mod.rs
+++ src/example/mod.rs
@@ -4,7 +4,8 @@ use prost::Message;
 include!(concat!(env!("OUT_DIR"), "/rspc.hello.rs"));
 
 pub fn hello_service(reqeust: Bytes) -> (u32, Bytes) {
+    let reqeust = HelloRequest::decode(&reqeust[..]).unwrap();
     let mut reply = HelloReply::default();
-    reply.msg = String::from("Lin");
+    reply.msg = format!("hello {}", reqeust.name);
     (0, reply.encode_to_vec().into())
 }
