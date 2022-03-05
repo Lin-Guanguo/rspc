feb345fddeef65700b9fe3a11b6d2e38ccfa6e0f
Lin-Guanguo <lin.guanguo2000@gmail.com>
2022-03-05 19:45:07 +0800

    server refactor move old file

--- src/protocol/service.rs
+++
@@ -1,9 +0,0 @@
-use bytes::Bytes;
-
-pub trait Service {
-    fn call_method(&self, fn_n: usize, reqeust: Bytes) -> (u32, Bytes);
-
-    fn method_names(&self) -> &'static [&'static str];
-
-    fn num_of_methods(&self) -> usize;
-}
--- src/server/service.rs
+++ src/server/service.rs
@@ -1,30 +1,9 @@
 use bytes::Bytes;
-use std::{
-    collections::HashMap,
-    sync::{Arc, RwLock},
-};
 
-pub type ServiceFn = fn(Bytes) -> (u32, Bytes);
+pub trait Service {
+    fn call_method(&self, fn_n: usize, reqeust: Bytes) -> (u32, Bytes);
 
-#[derive(Clone)]
-pub struct ServiceTable {
-    table: Arc<RwLock<HashMap<u32, ServiceFn>>>,
-}
+    fn method_names(&self) -> &'static [&'static str];
 
-impl ServiceTable {
-    pub fn new() -> Self {
-        Self {
-            table: Arc::new(RwLock::new(HashMap::new())),
-        }
-    }
-
-    pub fn insert(&mut self, method_id: u32, service_fn: ServiceFn) -> Option<ServiceFn> {
-        let mut map = self.table.write().unwrap();
-        map.insert(method_id, service_fn)
-    }
-
-    pub fn get(&self, method_id: u32) -> Option<ServiceFn> {
-        let map = self.table.read().unwrap();
-        map.get(&method_id).copied()
-    }
+    fn num_of_methods(&self) -> usize;
 }
