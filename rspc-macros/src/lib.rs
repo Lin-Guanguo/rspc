df272b892952be7d8f9a1d4034445a19a235715d
Lin-Guanguo <lin.guanguo2000@gmail.com>
2022-03-09 13:55:58 +0800

    add rspc-client macros start

--- rspc-macros/src/lib.rs
+++ rspc-macros/src/lib.rs
@@ -1,9 +1,19 @@
 extern crate proc_macro;
-use proc_macro::TokenStream;
-use syn::DeriveInput;
+use proc_macro::{TokenStream, TokenTree};
 
 #[proc_macro_attribute]
 pub fn rspc_client(attr: TokenStream, item: TokenStream) -> TokenStream {
-    println!("{:#?}", attr);
+    // let normal_rpc = vec![];
+    // let stream_rpc = vec![];
+    for ident in attr {
+        println!("{:?}", ident);
+        continue;
+        match ident {
+            TokenTree::Punct(_) => {}
+            TokenTree::Group(_) => todo!(),
+            TokenTree::Ident(_) => todo!(),
+            TokenTree::Literal(_) => todo!(),
+        }
+    }
     item
 }
--- rspc-macros/src/main.rs
+++ rspc-macros/src/main.rs
@@ -1,3 +1,8 @@
+use rspc_macros::rspc_client;
+
+#[rspc_client((rr, dd), "liter", a = 3, rpc1, rpc2, stream rpc3)]
+struct RspcClient {}
+
 fn main() {
     println!("hello world");
 }
